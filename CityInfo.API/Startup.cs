2f9b1bf22b0572213d36a78b937b3d73c9b0321c
DESKTOP-G6VSS41\Truong Dung <truongdung0502@hotmail.com>
2018-05-31 00:08:32 -0500

    add aspnet core mvc

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -35,6 +36,10 @@ namespace CityInfo.API
             }
 
             app.UseMvc();
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("asdf");
+            //});
         }
     }
 }
